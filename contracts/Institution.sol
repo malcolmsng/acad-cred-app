bf402f7819161890a46958f4625c51e50bfb2ebc
Remus <remuskwan23@gmail.com>
2023-03-09 20:00:32 +0800

    Revert "build: add prettier script"
    This reverts commit 871af836347e927294ccf11bb543c5ba9a4fbd3c.

--- contracts/Institution.sol
+++ contracts/Institution.sol
@@ -1,17 +1,17 @@
 pragma solidity >=0.5.0;
 
 contract Instituition {
-    struct institution {
-        string name;
-        string country;
-        string city;
-        string latitude;
-        string longitude;
-        address owner;
-    }
+  struct institution {
+    string name;
+    string country;
+    string city;
+    string latitude;
+    string longitude;
+    address owner;
+  }
 
-    uint256 public numInstitutions = 0;
-    mapping(uint256 => institution) public instituitions;
+  uint256 public numInstitutions = 0;
+  mapping(uint256 => institution) public instituitions;
 
-    constructor() public {}
+  constructor() public {}
 }
