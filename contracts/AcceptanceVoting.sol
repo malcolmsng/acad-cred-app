23b7381334d29afe31dda6326984b3318524958e
Malcolm Sng <malcolmsng99@gmail.com>
2023-03-20 13:03:03 +0800

    npm installed

--- contracts/AcceptanceVoting.sol
+++ contracts/AcceptanceVoting.sol
@@ -1,51 +1,47 @@
 pragma solidity ^0.5.0;
 
 contract AcceptanceVoting {
-    // member address => true if exist 
-    mapping(address => bool) committeeMembers;
-    // members => true if voted
-    mapping(address => bool) hasVoted;
-    uint32 committeeSize;
-    address committeeChairman;
-    // should we set a deadline to vote e.g. 1 week
-    uint votingDeadline;
-    VotingState currentState;
+  // member address => true if exist
+  mapping(address => bool) committeeMembers;
+  // members => true if voted
+  mapping(address => bool) hasVoted;
+  uint32 committeeSize;
+  address committeeChairman;
+  // should we set a deadline to vote e.g. 1 week
+  uint votingDeadline;
+  VotingState currentState;
 
-    enum VotingState {OPEN, CLOSED}
-    
-    // should the person who deploys the contract be the chairman?
-    // or should we allocate the chairman
-    constructor () public {
-        currentState = VotingState.CLOSED;
-    }
+  enum VotingState {
+    OPEN,
+    CLOSED
+  }
 
-    function vote() external {
+  // should the person who deploys the contract be the chairman?
+  // or should we allocate the chairman
+  constructor() public {
+    currentState = VotingState.CLOSED;
+  }
 
-    }
+  function vote() external {}
 
-    function openVote() external {
+  function openVote() external {}
 
-    }
+  function closeVote() external {}
 
-    function closeVote() external {
+  function distributeFee() external payable {}
 
-    }
+  // getters
+  function getCommitteeChairman() external view returns (address) {
+    return committeeChairman;
+  }
 
-    function distributeFee() external payable {
+  function getVotingDeadline() external view returns (uint) {
+    return votingDeadline;
+  }
 
-    }
-
-    // getters
-    function getCommitteeChairman() external view returns (address){
-        return committeeChairman;
-    }
-
-    function getVotingDeadline() external view returns (uint){
-        return votingDeadline;
-    }
-    // will return the index of the voting state 
-    // i.e. VotingState.OPEN == 0
-    function getVotingState() external view returns (VotingState){
-       return currentState;
-    }
+  // will return the index of the voting state
+  // i.e. VotingState.OPEN == 0
+  function getVotingState() external view returns (VotingState) {
+    return currentState;
+  }
 }
